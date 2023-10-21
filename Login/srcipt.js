95cdf36ba0dcacbbe51a5ef736c2017cc9196068
Chetan Navnath Padhen <padhenchetan@gmail.com>
2023-10-21 12:22:08 +0530

    email validate

--- Login/srcipt.js
+++ Login/srcipt.js
@@ -1,8 +1,24 @@
-const logi=document.getElementById('useremail');
-function loginFun(){
 
-    if(logi.value==" ")
-    {
-        alert('Email Is Required');
-    }
+const logi = document.getElementById('useremail');
+const logiBut = document.getElementById('loginBut');
+
+logiBut.addEventListener('click', () => {
+if (!logi.value) {
+alert('Email Is Required');
+logi.value = "";
+} else {
+var ans = logi.value;
+if (ans.length < 4) {
+  alert('Invalid Email');
+  logi.value = ""; 
+} else {
+  if (/\.com$/i.test(ans)) {
+    
+  } else {
+      alert('Invalid email');
+      logi.value = ""; 
+  }
 }
+}
+});
+
