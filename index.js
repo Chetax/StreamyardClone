6af353e17c5a4ed35bbe0cb9c4c28a3d286db99b
Chetan Navnath Padhen <padhenchetan@gmail.com>
2024-06-03 13:42:49 +0530

    Corrected Public url

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ const app=express();
 const {restricToLoggedInUserOnly}=require('./middleware/auth');
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
+
+
 const Userroute=require('./route/user');
 const staticRoute=require('./route/staticRoute');
 const strem=require('./route/strem');
+
 const {connectotmongodb}=require('./connect');
 connectotmongodb('mongodb://localhost/User').then(()=>{console.log("Mongose Connect");})
 app.set("view engine","ejs");
@@ -15,7 +18,7 @@ app.set('views',path.resolve('./view'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use('/Public',express.static('../Public'));
+app.use('/Public',express.static('Public'));
 app.use('/',staticRoute);     
 app.use('/user',Userroute);
 app.use('/stream',restricToLoggedInUserOnly,strem)
