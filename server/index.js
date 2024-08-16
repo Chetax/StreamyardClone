0ca74fcc9e6a134350c4275f47bf07842c121392
Chetan Navnath Padhen <padhenchetan@gmail.com>
2024-08-16 18:33:17 +0530

    Added Few Connection of socket

--- server/index.js
+++ server/index.js
@@ -1,18 +1,38 @@
-const express=require('express')
-const {Server, Socket}=require('socket.io')
-const bodyParser=require('body-parser')
+const express = require('express');
+const { Server } = require('socket.io');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const http = require('http');
 
-const app=express();
-const io=new Server()
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
+// Create an HTTP server and wrap the express app
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    credentials: true
+  }
+});
 
-app.use(bodyParser.json())
+ const emailTosoketMap=new Map();
 
-io.on("connection",socket=>{
+io.on("connect", (socket) => {
 
-})
+  socket.on("join-room",(data)=>{
+    const {roomId,emailId}=data;
+    console.log("User ",emailId ," Joinded Room ", roomId)
+      emailTosoketMap.set({emailId,roomId});
+      socket.join(roomId);
+      socket.broadcast.to(roomId).emit("user-joined",{emailId})
+      socket.emit("joinedroom",roomId);
+  })
+  console.log("User is connected with socket ID:", socket.id);
+});
 
-io.listen(8001);
-app.listen(8000,()=>{
-    console.log("Server Is Started On Port 4000"); 
-})
+server.listen(8000, () => {
+  console.log("Server is started on port 8000");
+});
