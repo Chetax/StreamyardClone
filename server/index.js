4f13c176299e15da49fb8ede1e6181fdc8a7eb20
Chetan Navnath Padhen <padhenchetan@gmail.com>
2024-08-17 16:56:21 +0530

    Added Webrtc

--- server/index.js
+++ server/index.js
@@ -19,17 +19,37 @@ const io = new Server(server, {
 });
 
  const emailTosoketMap=new Map();
+ const socketToemailMap=new Map();
+
 
 io.on("connect", (socket) => {
 
   socket.on("join-room",(data)=>{
     const {roomId,emailId}=data;
+
     console.log("User ",emailId ," Joinded Room ", roomId)
-      emailTosoketMap.set({emailId,roomId});
-      socket.join(roomId);
-      socket.broadcast.to(roomId).emit("user-joined",{emailId})
+
+      emailTosoketMap.set(emailId,socket.id);
+      socketToemailMap.set(socket.id,emailId);
       socket.emit("joinedroom",roomId);
+      socket.join(roomId);
+      socket.broadcast.to(roomId).emit("user-joined",emailId)
   })
+
+  socket.on('call-user',(data)=>{
+    const {emailId,offer} =data;
+    const fromEmail=socketToemailMap.get(socket.id)
+    const soketid=emailTosoketMap.get(emailId)
+    io.to(soketid).emit('incoming-call',{from:fromEmail,offer:offer});
+  })
+
+  socket.on('call-accepted',(data)=>{
+    const {from , ans}=data;
+    const socketid=emailTosoketMap(from)
+    socketid.emit("call-accepted",{ans})
+    
+  })
+
   console.log("User is connected with socket ID:", socket.id);
 });
 
