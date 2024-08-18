522f298b31dc89ed1708ae63ef7a1e08464772e1
Chetan Navnath Padhen <padhenchetan@gmail.com>
2024-08-18 19:07:05 +0530

    Done With Webrtc Logic

--- server/index.js
+++ server/index.js
@@ -1,52 +1,50 @@
-const express = require('express');
-const { Server } = require('socket.io');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const http = require('http');
+const express = require("express");
+const { Server } = require("socket.io");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const http = require("http");
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-// Create an HTTP server and wrap the express app
+
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:3000",
     methods: ["GET", "POST"],
-    credentials: true
-  }
+    credentials: true,
+  },
 });
 
- const emailTosoketMap=new Map();
- const socketToemailMap=new Map();
-
+const emailTosoketMap = new Map();
+const socketToemailMap = new Map();
 
 io.on("connect", (socket) => {
-
-  socket.on("join-room",(data)=>{
-    const {roomId,emailId}=data;
-
-    console.log("User ",emailId ," Joinded Room ", roomId)
-
-      emailTosoketMap.set(emailId,socket.id);
-      socketToemailMap.set(socket.id,emailId);
-      socket.emit("joinedroom",roomId);
-      socket.join(roomId);
-      socket.broadcast.to(roomId).emit("user-joined",emailId)
-  })
-
+  socket.on("join-room", (data) => {
+    const { roomId, emailId } = data;
+    console.log("User ", emailId, " Joinded Room ", roomId);
+    emailTosoketMap.set(emailId, socket.id);
+    socketToemailMap.set(socket.id, emailId);
+    socket.emit("joinedroom", { roomId, emailId });
+    socket.join(roomId);
+    socket.broadcast.to(roomId).emit("user-joined", { emailId });
+  });
+  
   socket.on('call-user',(data)=>{
-    const {emailId,offer} =data;
-    const fromEmail=socketToemailMap.get(socket.id)
-    const soketid=emailTosoketMap.get(emailId)
-    io.to(soketid).emit('incoming-call',{from:fromEmail,offer:offer});
+      const { emailId, offer}=data;
+      console.log("call-user Function get called",{emailId,offer});
+      const socketid=emailTosoketMap.get(emailId)
+      const from=socketToemailMap.get(socket.id)
+      socket.to(socketid).emit("incomming-call",{from,offer})
   })
 
   socket.on('call-accepted',(data)=>{
-    const {from , ans}=data;
-    const socketid=emailTosoketMap(from)
-    socketid.emit("call-accepted",{ans})
+    console.log("Call-accepted function get called from client and printing in server");
+    const {from,answer}=data;
+    const socketid=emailTosoketMap.get(from)
+    socket.to(socketid).emit("call-accepted",{answer})
     
   })
 
