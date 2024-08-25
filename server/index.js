dd423446f174dbea08b7cc80eb72f04e24061b65
Chetan Navnath Padhen <padhenchetan@gmail.com>
2024-08-25 15:02:00 +0530

    Complete WebRTC protocol implementation

--- server/index.js
+++ server/index.js
@@ -1,56 +1,38 @@
-const express = require("express");
 const { Server } = require("socket.io");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const http = require("http");
 
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
-
-
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-    credentials: true,
-  },
+const io = new Server(8000, {
+  cors: true,
 });
 
-const emailTosoketMap = new Map();
-const socketToemailMap = new Map();
+const emailToSocketIdMap = new Map();
+const socketidToEmailMap = new Map();
 
-io.on("connect", (socket) => {
-  socket.on("join-room", (data) => {
-    const { roomId, emailId } = data;
-    console.log("User ", emailId, " Joinded Room ", roomId);
-    emailTosoketMap.set(emailId, socket.id);
-    socketToemailMap.set(socket.id, emailId);
-    socket.emit("joinedroom", { roomId, emailId });
-    socket.join(roomId);
-    socket.broadcast.to(roomId).emit("user-joined", { emailId });
+io.on("connection", (socket) => {
+  console.log(`Socket Connected`, socket.id);
+  socket.on("room:join", (data) => {
+    const { email, room } = data;
+    emailToSocketIdMap.set(email, socket.id);
+    socketidToEmailMap.set(socket.id, email);
+    io.to(room).emit("user:joined", { email, id: socket.id });
+    socket.join(room);
+    io.to(socket.id).emit("room:join", data);
   });
+
+  socket.on("user:call", ({ to, offer }) => {
+    io.to(to).emit("incomming:call", { from: socket.id, offer });
+  });
+
+  socket.on("call:accepted", ({ to, ans }) => {
+    io.to(to).emit("call:accepted", { from: socket.id, ans });
+  });
+
+  socket.on("peer:nego:needed", ({ to, offer }) => {
+
+    io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
+  });
+
+  socket.on("peer:nego:done", ({ to, ans }) => {
   
-  socket.on('call-user',(data)=>{
-      const { emailId, offer}=data;
-      console.log("call-user Function get called",{emailId,offer});
-      const socketid=emailTosoketMap.get(emailId)
-      const from=socketToemailMap.get(socket.id)
-      socket.to(socketid).emit("incomming-call",{from,offer})
-  })
-
-  socket.on('call-accepted',(data)=>{
-    console.log("Call-accepted function get called from client and printing in server");
-    const {from,answer}=data;
-    const socketid=emailTosoketMap.get(from)
-    socket.to(socketid).emit("call-accepted",{answer})
-    
-  })
-
-  console.log("User is connected with socket ID:", socket.id);
-});
-
-server.listen(8000, () => {
-  console.log("Server is started on port 8000");
+    io.to(to).emit("peer:nego:final", { from: socket.id, ans });
+  });
 });
